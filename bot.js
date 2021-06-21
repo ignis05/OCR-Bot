a416bfc77f0ebf21e9e439262e78d0eab50a151f
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-06-21 19:09:01 +0200

    Set language and scale

--- bot.js
+++ bot.js
@@ -1,24 +1,24 @@
-const Discord = require('discord.js')
 const ocrSpace = require('ocr-space-api-wrapper')
 
-const token = require('token.js').token
+const Discord = require('discord.js')
+const intents = new Discord.Intents(Discord.Intents.NON_PRIVILEGED)
+const client = new Discord.Client({ intents })
 
-const client = new Discord.Client()
+const token = require('./token.json').token
 
 client.on('ready', () => {
-	client.users.fetch(botOwnerID).then(owner => {
-		owner.send('Ready!')
-	})
 	console.log('Ready!')
 })
 
 client.on('message', async msg => {
+	if (msg.channel.id !== '467317385251258378') return // limit to one channel
 	if (msg.attachments.size > 0) {
-		res = await ocrSpace(msg.attachments.first().url, { language: 'pol' })
+		res = await ocrSpace(msg.attachments.first().url, { language: 'pol', scale: 'true' })
 		var resmsg = res?.ParsedResults[0]?.ParsedText
-		if (resmsg) inter.editReply(`Result:\n\`\`\`${resmsg}\`\`\``)
+		if (resmsg) msg.reply(`\`\`\`${resmsg}\`\`\``, { allowedMentions: { users: [] } })
 		else console.log('ocr failed')
 	}
+
 })
 
 client.on('error', console.error)
