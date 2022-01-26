5420fd35b82afb85a10dae0ea046b8ade644a047
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-01-26 19:34:26 +0100

    chore: reload on changes in "modules" dir

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
 			args: 'one two',
 			instances: 1,
 			autorestart: true,
-			watch: ['bot.js', 'commands'],
+			watch: ['bot.js', 'commands', 'modules'],
 			max_memory_restart: '1G',
 			env: {
 				NODE_ENV: 'development',
