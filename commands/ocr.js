704b80a1269c42088ea1d9b79caa9ac7a9dc2225
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-01-27 15:15:43 +0100

    refactor: ocr interaction

--- commands/ocr.js
+++ commands/ocr.js
@@ -12,25 +12,24 @@ module.exports = {
 	async handler(inter) {
 		let msg = inter.options.getMessage('message')
 
-		if (msg.attachments.size > 0) {
-			/** @type {MessageAttachment} **/
-			let attachment = msg.attachments.first()
-			if (/\.(png|jpg)$/i.test(attachment.name)) {
-				await inter.deferReply()
+		if (msg.attachments.size == 0) return inter.reply({ content: `This message has no attachments.`, ephemeral: true })
+		/** @type {MessageAttachment} **/
+		let attachment = msg.attachments.first()
+		if (!/\.(png|jpg)$/i.test(attachment.name)) return inter.reply({ content: `The attachment is not a png/jpg file.`, ephemeral: true })
 
-				console.log(`running ocr in #${msg.channel.name}`)
+		await inter.deferReply()
 
-				let limit = 1950 - msg.url.length
-				/** @type {multiOcrRes} **/
-				var res = await multiOcr(attachment)
-				if (res.success) {
-					let resMsg = res.text
-					if (!resMsg) return inter.editReply({ content: `No text found` })
-					// trim message if it's too long
-					if (resMsg.length > limit) resMsg = resMsg.substring(0, limit)
-					inter.editReply(`[link](${msg.url})\n\`\`\`${resMsg}\`\`\``)
-				} else inter.editReply(`Ocr failed.`)
-			} else inter.reply({ content: `The attachment is not a png/jpg file.`, ephemeral: true })
-		} else inter.reply({ content: `This message has no attachments.`, ephemeral: true })
+		console.log(`processing ocr interaction in #${msg.channel.name}`)
+
+		let limit = 1950 - msg.url.length
+		/** @type {multiOcrRes} **/
+		var res = await multiOcr(attachment)
+		if (res.success) {
+			let resMsg = res.text
+			if (!resMsg) return inter.editReply({ content: `No text found` })
+			// trim message if it's too long
+			if (resMsg.length > limit) resMsg = resMsg.substring(0, limit)
+			inter.editReply(`[link](${msg.url})\n\`\`\`${resMsg}\`\`\``)
+		} else inter.editReply(`Ocr failed.`)
 	},
 }
